fca76a8ed07890f3644072f582fd3b3bdc96a308
Igor Martynov <igor.martynov@intel.com>
2015-12-29 18:57:02 +0300

    Refactor PETC parser code

--- petool/src/petc_parser.c
+++ petool/src/petc_parser.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
@@ -6,6 +7,7 @@
 #define MAX_ERROR_LEN 5000
 #define MAX_SIZE_LEN 10
 #define MAX_TYPE_LEN 10
+#define MAX_VALUE_LEN 10
 
 static FILE *file = NULL;
 static int c = '\0';
@@ -17,186 +19,200 @@ static char field_size[MAX_SIZE_LEN + 1] = "";
 static char field_type[MAX_TYPE_LEN + 1] = "";
 static char field_name[MAX_NAME_LEN + 1] = "";
 static char description[MAX_DESCR_LEN + 1] = "";
+static size_t descr_idx = 0;
+static char value_name[MAX_NAME_LEN + 1] = "";
+static char value_num[MAX_VALUE_LEN + 1] = "";
 
 static char error[MAX_ERROR_LEN + 1] = "";
 static int error_pos = 0;
 static bool status = true;
 
-#define set_parse_error(FORMAT, ...)                                \
-do {                                                                \
-    int written = sprintf_s(                                        \
-        error + error_pos,                                          \
-        MAX_ERROR_LEN + 1 - error_pos,                              \
-        "%d:%d " FORMAT,                                            \
-        line_number, sym_number, __VA_ARGS__);                      \
-    error_pos += written;                                           \
-    status = false;                                                 \
+// Raise an error during file parsing
+#define err(FORMAT, ...)                                                        \
+do {                                                                            \
+    error_pos += sprintf_s(error + error_pos, MAX_ERROR_LEN + 1 - error_pos,    \
+        "%d:%d " FORMAT, line_number, sym_number, __VA_ARGS__);                 \
+    status = false;                                                             \
 } while (0)
 
-#define check_status() do { if (!status) { return false; } } while (0)
+// Check current parser status, and return immediately, if something is wrong
+#define check_status()                                                          \
+do {                                                                            \
+    if (!status) {                                                              \
+        return;                                                                 \
+    }                                                                           \
+} while (0)
 
+
+/*
+ * Scanner function. Obtains 1 character at a time.
+ * Performes translation:
+ *     any kind of end-of-line (CR, LF, CRLF) -> \n
+ * Other characters are delivered as-is.
+*/
 void get_char()
 {
+    // Get next character from the file
     c = getc(file);
-    sym_number++;
+    if (c == '\r') {
+        // Skip the \r in \r\n sequence
+        int c2 = getc(file);
+        if (c2 != '\n') {
+            fseek(file, -1, SEEK_CUR);
+        }
+        c = '\n';
+    }
+
+    // Advance current line and symbol
     if (c == '\n') {
         sym_number = 0;
         line_number++;
+    } else {
+        sym_number++;
     }
 }
 
-bool is_letter()
-{
-    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
-}
 
-bool is_dec_digit()
-{
-    return (c >= '0' && c <= '9');
-}
+/*
+ * Checker functions for various character classes
+ * Checkers operate on currently observed character ('c').
+ * A checker can use other checkers.
+*/
 
-bool is_hex_digit()
-{
-    return ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'));
-}
+#define CHECKER(NAME, CONDITION) bool NAME() { return (CONDITION); }
 
-bool is_whitespace()
-{
-    return (c == ' ' || c == '\t' || c == '\v' || c == '\r' || c == '\n');
-}
+CHECKER( is_letter,          c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z'                         )
+CHECKER( is_dec_digit,       c >= '0' && c <= '9'                                                 )
+CHECKER( is_hex_digit,       c >= '0' && c <= '9' || c >= 'A' && c <= 'F' || c >= 'a' && c <= 'f' )
+CHECKER( is_whitespace,      c == ' ' || c == '\t' || c == '\v' || c == '\r' || c == '\n'         )
+CHECKER( is_line_space,      c == ' ' || c == '\t'                                                )
+CHECKER( is_eol,             c == '\r' || c == '\n'                                               )
+CHECKER( is_non_eol,         c != '\r' && c != '\n'                                               )
+CHECKER( is_alnum,           is_letter() || is_dec_digit()                                        )
+CHECKER( is_value_name_char, is_letter() || is_dec_digit() || c == '_'                            )
+CHECKER( is_table_name_char, is_letter() || is_dec_digit() || c == ' '                            )
 
-bool is_line_space()
-{
-    return (c == ' ' || c == '\t');
-}
+#undef CHECKER
 
-bool is_eol()
-{
-    return (c == '\r' || c == '\n');
-}
 
-bool is_char(char ch)
-{
-    return (c == ch);
-}
+/*
+ * Low-level parsing functions.
+*/
 
-bool parse_char(char ch)
+// Parse a single pre-defined character
+void parse_char(char ch)
 {
     check_status();
-    if (!is_char(ch)) {
-        set_parse_error("Expected: %c\n", ch);
-        return false;
+    if (c != ch) {
+        err("Expected: %c\n", ch);
+        return;
     }
     get_char();
-    return status;
 }
 
-bool parse_string(char str[])
+// Parse a single character using a checker function
+void parse_char_check(bool (*checker)(), char *expected_descr)
+{
+    check_status();
+    if (checker()) {
+        get_char();
+        return;
+    }
+    err("Expected: %s\n", expected_descr);
+}
+
+// Parse a sequence of characters
+void parse_string(char str[])
 {
     check_status();
     for (char *p = str; *p != '\0'; p++) {
         parse_char(*p);
     }
-    return status;
 }
 
-bool parse_eol()
+
+/*
+ * Functions for retrieving values from file being parsed
+*/
+
+// Read a single character from the file being parsed, using a checker.
+void read_char(bool (*checker)(), char *dst, char *name)
+{
+    if (checker()) {
+        *dst = c;
+    }
+    parse_char_check(checker, name);
+}
+
+// Read a value from the file being parsed to a fixed-length buffer.
+// Returns: Number of characters written to buffer, not counting the final
+//          null character.
+size_t read_value(bool (*checker)(), char *dst, size_t max_len, char *name)
+{
+    size_t i = 0;
+    while (checker()) {
+        if (i >= max_len) {
+            err("Error: %s is too long\n", name);
+            break;
+        }
+        dst[i++] = c;
+        get_char();
+    }
+    dst[i] = '\0';
+    return i;
+}
+
+
+/*
+** Parsing functions for grammar non-terminals
+*/
+
+void parse_eol()
 {
     check_status();
-
     if (c == '\n') {
         get_char();
-        return true;
     } else if (c == '\r') {
         get_char();
         if (c == '\n') {
             get_char();
         }
-        return true;
+    } else {
+        err("Expected: %s\n", "newline");
     }
-    set_parse_error("Expected: %s\n", "newline");
-    return status;
 }
 
-bool accept_letter()
-{
-    check_status();
-    if (is_letter()) {
-        get_char();
-        return true;
-    }
-    return false;    
-}
-
-bool parse_letter()
-{
-    check_status();
-    if (is_letter()) {
-        get_char();
-        return true;
-    }
-    set_parse_error("Expected: %s\n", "letter");
-    return false;
-}
-
-bool parse_decimal_digit()
-{
-    check_status();
-    if (c >= '0' && c <= '9') {
-        get_char();
-        return true;
-    }
-    set_parse_error("Expected: %s\n", "decimal digit");
-    return status;
-}
-
-bool parse_hexadecimal_digit()
-{
-    check_status();
-    if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f')) {
-        get_char();
-        return true;
-    }
-    set_parse_error("Expected: %s\n", "hexadecimal digit");
-    return status;
-}
-
-bool parse_hexadecimal_number()
+void parse_value_number()
 {
     check_status();
     parse_string("0x");
-    parse_hexadecimal_digit();
-    while (is_hex_digit()) {
-        parse_hexadecimal_digit();
+
+    size_t i = 0;
+    if (is_hex_digit()) {
+        value_num[i++] = c;
     }
+    parse_char_check(is_hex_digit, "hex digit");
+
+    read_value(is_hex_digit, value_num + 1, MAX_VALUE_LEN - 1, "value");
 
     if (!status) {
-        set_parse_error("Expected: %s\n", "hexadecimal number");
+        err("Expected: %s\n", "hexadecimal number");
     }
-    return status;
 }
 
-bool parse_field_size()
+void parse_field_size()
 {
     check_status();
     size_t i = 0;
     if (is_dec_digit()) {
         field_size[i++] = c;
     }
-    parse_decimal_digit();
-    while (is_dec_digit()) {
-        if (i >= MAX_SIZE_LEN) {
-            set_parse_error("Error: %s\n", "field size is too long");
-            break;
-        }
-        field_size[i++] = c;
-        get_char();
-    }
-    field_size[i] = '\0';
-    return status;
+    parse_char_check(is_dec_digit, "decimal digit");
+
+    read_value(is_dec_digit, field_size + 1, MAX_SIZE_LEN - 1, "field size");
 }
 
-bool parse_field_separator()
+void parse_field_separator()
 {
     check_status();
     while (is_line_space()) {
@@ -206,10 +222,9 @@ bool parse_field_separator()
     while (is_line_space()) {
         get_char();
     }
-    return status;
 }
 
-bool parse_separator()
+void parse_separator()
 {
     check_status();
     parse_char('-');
@@ -217,83 +232,62 @@ bool parse_separator()
         get_char();
     }
     parse_eol();
-    return status;
 }
 
-bool parse_description_line()
+void parse_description_line()
 {
     check_status();
-    while (c != '\n' && c != '\r') {
-        get_char();
-    }
+    size_t written = read_value(is_non_eol, description + descr_idx, MAX_DESCR_LEN - descr_idx, "description");
+    descr_idx += written;
     parse_eol();
-    return status;
 }
 
-bool parse_description()
+void parse_description()
 {
     check_status();
 
-    size_t i = 0;
-    while (c != '\n' && c != '\r') {
-        if (i >= MAX_DESCR_LEN) {
-            set_parse_error("Error: %s\n", "description is too long");
-            break;
-        }
-        description[i++] = c;
-        get_char();
-    }
-    parse_eol();
-
+    descr_idx = 0;
+    parse_description_line();
     while (c == ' ') {
-        if (i >= MAX_DESCR_LEN) {
-            set_parse_error("Error: %s\n", "description is too long");
+        if (descr_idx >= MAX_DESCR_LEN) {
+            err("Error: %s\n", "description is too long");
             break;
         }
-        description[i++] = ' ';
+        description[descr_idx++] = ' ';
         parse_char(' ');
         while (c == ' ') {
             get_char();
         }
-
-        while (c != '\n' && c != '\r') {
-            if (i >= MAX_DESCR_LEN) {
-                set_parse_error("Error: %s\n", "description is too long");
-                break;
-            }
-            description[i++] = c;
-            get_char();
-        }
-        parse_eol();
+        parse_description_line();
     }
-    description[i] = '\0';
-
-    return status;
+    description[descr_idx] = '\0';
 }
 
-bool parse_value_name()
+void parse_value_name()
 {
     check_status();
-    parse_letter();
-    check_status();
-    while (is_letter() || is_dec_digit() || c == '_') {
-        get_char();
+    size_t i = 0;
+    if (is_letter()) {
+        value_name[i++] = c;
     }
-    return status;
+    parse_char_check(is_letter, "letter");
+    read_value(is_value_name_char, value_name + 1, MAX_NAME_LEN - 1, "value");
 }
 
-bool parse_value()
+void parse_value()
 {
     check_status();
     parse_value_name();
     parse_field_separator();
-    parse_hexadecimal_number();
+    parse_value_number();
     parse_field_separator();
     parse_description();
-    return status;
+
+    printf("    VALUE: %s (0x%s)\n", value_name, value_num);
+    printf("           %s\n", description);
 }
 
-bool parse_field_definition()
+void parse_field_definition()
 {
     check_status();
     parse_separator();
@@ -303,30 +297,21 @@ bool parse_field_definition()
         parse_value();
     }
     parse_separator();
-    return status;
 }
 
-bool parse_field_name()
+void parse_field_name()
 {
     check_status();
     size_t i = 0;
     if (is_letter()) {
         field_name[i++] = c;
     }
-    parse_letter();
-    while (is_letter() || is_dec_digit()) {
-        if (i >= MAX_NAME_LEN) {
-            set_parse_error("Error: %s\n", "field name is too long");
-            break;
-        }
-        field_name[i++] = c;
-        get_char();
-    }
-    field_name[i] = '\0';
-    return status;
+    parse_char_check(is_letter, "letter");
+
+    read_value(is_alnum, field_name + 1, MAX_NAME_LEN - 1, "field name");
 }
 
-bool parse_field_type()
+void parse_field_type()
 {
     check_status();
     if (c == 'E') {
@@ -346,12 +331,11 @@ bool parse_field_type()
         check_status();
         strncpy_s(field_type, MAX_TYPE_LEN + 1, "FLAG", MAX_TYPE_LEN);
     } else {
-        set_parse_error("Expected: %s\n", "field type");
+        err("Expected: %s\n", "field type");
     }
-    return status;
 }
 
-bool parse_field()
+void parse_field()
 {
     check_status();
     parse_field_size();
@@ -362,16 +346,11 @@ bool parse_field()
     parse_field_separator();
     parse_description();
 
-    printf("FIELD:\n");
-    printf("  Size: %s\n", field_size);
-    printf("  Type: %s\n", field_type);
-    printf("  Name: %s\n", field_name);
-    printf("  Description: %s\n", description);
-
-    return status;
+    printf("    FIELD: %s (%s, %s bytes)\n", field_name, field_type, field_size);
+    printf("           %s\n", description);
 }
 
-bool parse_table_definition()
+void parse_table_definition()
 {
     check_status();
     parse_separator();
@@ -381,10 +360,9 @@ bool parse_table_definition()
         parse_field();
     }
     parse_separator();
-    return status;
 }
 
-bool parse_table_name()
+void parse_table_name()
 {
     check_status();
 
@@ -392,22 +370,12 @@ bool parse_table_name()
     if (is_letter()) {
         table_name[i++] = c;
     }
-    parse_letter();
+    parse_char_check(is_letter, "letter");
 
-    while (is_letter() || is_dec_digit() || is_char(' ')) {
-        if (i >= MAX_NAME_LEN) {
-            set_parse_error("Error: %s\n", "table name is too long");
-            break;
-        }
-        table_name[i++] = c;
-        get_char();
-    }
-    table_name[i] = '\0';
-
-    return status;
+    read_value(is_table_name_char, table_name + 1, MAX_NAME_LEN - 1, "table name");
 }
 
-bool parse_definition()
+void parse_definition()
 {
     check_status();
     parse_char('[');
@@ -419,23 +387,32 @@ bool parse_definition()
 
     if (is_eol()) {
         parse_eol();
+
+        printf("Table: %s\n", table_name);
+
         parse_table_definition();
+
+        printf("\n");
     } else {
         parse_char(':');
         while (is_line_space()) {
             get_char();
         }
         parse_field_name();
+
+        printf("Table [%s] field: %s\n", table_name, field_name);
+
         while (is_line_space()) {
             get_char();
         }
         parse_eol();
         parse_field_definition();
+
+        printf("\n");
     }
-    return status;
 }
 
-bool parse_config()
+void parse_config()
 {
     check_status();
     while (is_whitespace()) {
@@ -450,19 +427,23 @@ bool parse_config()
     }
 
     if (c != EOF) {
-        set_parse_error("Unexpected symbol: %c\n", c);
+        err("Unexpected symbol: %c\n", c);
     }
-
-    return status;
 }
 
+
+/*
+** Parsing interface
+*/
 
 void parse_file(const char *fname)
 {
     fopen_s(&file, fname, "r");
     get_char();
 
-    if (!parse_config()) {
+    parse_config();
+
+    if (!status) {
         fprintf(stderr, "%s", error);
     }
 
